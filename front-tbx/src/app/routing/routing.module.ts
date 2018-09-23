4d044b1748ab346dfb9b9b59e5724595bd014226
PauMB <paumb85@gmail.com>
2018-09-23 16:39:55 +0200

    se introduce la ruta account

--- front-tbx/src/app/routing/routing.module.ts
+++ front-tbx/src/app/routing/routing.module.ts
@@ -4,12 +4,14 @@ import {WelcomeComponent} from "../components/welcome/welcome.component";
 import {CollaresComponent} from "../components/collares/collares.component";
 import {AbrigosComponent} from "../components/abrigos/abrigos.component";
 import {ShoppingComponent} from "../components/shopping/shopping.component";
+import {AccountComponent} from "../components/account/account.component";
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent},
   { path: 'collares', component: CollaresComponent},
   { path: 'abrigos', component: AbrigosComponent},
-  { path: 'shopping', component: ShoppingComponent}
+  { path: 'shopping', component: ShoppingComponent},
+  { path: 'account', component: AccountComponent}
 ];
 
 @NgModule({
